f51ad74258e990bb7d3a88b8782aba2c97b127c9
Anand Sunderrajan <36977554+AnandSunderrajan@users.noreply.github.com>
2021-12-28 07:47:47 -0600

    Create spectral_normalization.py

---
+++ gan/spectral_normalization.py
@@ -0,0 +1,64 @@
+import torch
+from torch.optim.optimizer import Optimizer, required
+
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch import nn
+from torch import Tensor
+from torch.nn import Parameter
+
+def l2normalize(v, eps=1e-12):
+    return F.normalize(v, p=2, dim=0, eps=eps)
+    
+
+
+class SpectralNorm(nn.Module):
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        self._make_params()
+
+    def _update_u_v(self):
+        #Step1
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        #Step2
+        for i in range(self.power_iterations):
+            vl2 = torch.mv(torch.t(w.view(w.data.shape[0], -1).data), u.data)
+            ul2 = torch.mv(w.view(w.data.shape[0],-1).data, v.data)
+            v.data = l2normalize(vl2)
+            u.data = l2normalize(ul2)
+
+        #Step3
+        spectralnorm = u.dot(w.view(w.data.shape[0], -1).mv(v))
+
+        #Step4
+        setattr(self.module, self.name, w / spectralnorm.expand_as(w))
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = l2normalize(u.data)
+        v.data = l2normalize(v.data)
+        w_bar = Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
+
